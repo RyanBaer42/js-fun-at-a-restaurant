13fc1ffb1f7b530cb840fe3d75d2b9130f8cc2fe
Ryan Baer <ryanjbaer@outlook.com>
2022-10-12 16:54:19 -0600

    Complete meal challenges

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,42 @@
-function nameMenuItem() {
+function nameMenuItem(nameMenuItem) {
+  return `Delicious ${nameMenuItem}`
+};
 
+function createMenuItem(nameMenuItem, itemPrice, typeOfItem) {
+  var menuItem = {name:nameMenuItem, price:itemPrice, type: typeOfItem }
+  return menuItem
+};
+
+function addIngredients(newIngredient, ingredients){
+  if (!ingredients.includes(newIngredient)){
+    ingredients.push(newIngredient)
+  }
+  return ingredients
 }
 
+function formatPrice(initialPrice){
+  return `$${initialPrice}`
+}
+
+function decreasePrice(initialPrice){
+  var decreasedValue = initialPrice - initialPrice *.1;
+  return decreasedValue
+}
+
+function createRecipe(title, ingredients, menuItemType){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
