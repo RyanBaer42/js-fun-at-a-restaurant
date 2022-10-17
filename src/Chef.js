1ca1ca0097c115c9affe03c637ae376f0f6f7b6a
Ryan Baer <ryanjbaer@outlook.com>
2022-10-16 20:04:15 -0600

    Complete half of chef challenges

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,21 @@
 class Chef {
-  constructor() {
-    
+  constructor(name,restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(name, ifMorning){
+    if (ifMorning === true){
+      return `Good morning, ${name}!`
+    }
+    return `Hello, ${name}!`
+  }
+  checkForFood(foodItem){
+    if (this.restaurant.menus.breakfast.name.includes(foodItem.name)){
+      return `Yes, we're serving ${foodItem.name} today!`
+    }
   }
 }
 
-module.exports = Chef;
+
+module.exports = Chef
